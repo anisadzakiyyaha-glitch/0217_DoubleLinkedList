56cd7ffddb2d43897daa8414fe734e08f5ef12b7
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-28 13:19:27 +0700

    tambah fungsi main dan tampilan menu

--- TugasDLL.cpp
+++ TugasDLL.cpp
@@ -211,4 +211,26 @@ public:
             cout << "Roll Number: " << current->noMhs << endl; 
         }  
     }
-};
+}; 
+
+int main()
+{ 
+    DoubleLinkedList list; 
+    char choice;
+
+    do
+    {
+
+        cout << "\nMenu:\n"; 
+        cout << "1. Add Record\n"; 
+        cout << "2. Delete Record\n"; 
+        cout << "3. View Ascending\n"; 
+        cout << "4. View Descending\n";                 
+        cout << "5. Search Record\n"; 
+        cout << "6. Exit\n"; 
+        cout << "5. Enter your choice: "; 
+
+        cin >> choice;
+
+    } while (choice != '6');
+}
